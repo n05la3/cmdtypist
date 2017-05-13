bffa681be242cb7008ac4d059a46623927ff9237
Calson Noah <chiacalson@gmail.com>
2017-05-13 21:49:16 +0100

    completed install script

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 #!home/.bashrc
 #Creates a symbolic link with begin to cmdtypist directory.
-echo "Enter password to create a link for cmdtypist and access it from any directory"
-sudo echo >> $HOME/.bashrc&&
-echo "#symbolic link for cmdtypist">> $HOME/.bashrc&&
-echo "alias begin='cd $HOME/cmdtypistversion1.0/'">> $HOME/.bashrc&&
-echo "alias cmdtypist='$HOME/cmdtypistversion1.0/cmdtypist'">> $HOME/.bashrc&&
+echo "Enter password to create a link for cmdtypist and access it from any directory";
+gcc cmdtypist.c -o cmdtypist&
+sudo echo >> $HOME/.bashrc&
+echo "#symbolic link for cmdtypist">> $HOME/.bashrc&
+echo "alias begin='cd $HOME/cmdtypistversion1.0/'">> $HOME/.bashrc&
+echo "alias cmdtypist='$HOME/cmdtypistversion1.0/cmdtypist'">> $HOME/.bashrc&
 cp -r ../cmdtypistversion1.0 $HOME/cmdtypistversion1.0/ 
