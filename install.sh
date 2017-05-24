45fd26ae8bbdfc9a20f0521e3eeb58d2a717a17c
Calson Noah <chiacalson@gmail.com>
2017-05-24 18:28:55 +0100

    Makes directory and copy files to it

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
-#!home/.bashrc
+#!/bin/bash
 #Creates a symbolic link with begin to cmdtypist directory.
-echo "Enter password to create a link for cmdtypist and access it from any directory";&&
-gcc cmdtypist.c -o cmdtypist&&
-sudo echo >> $HOME/.bashrc&&
-echo "#symbolic link for cmdtypist">> $HOME/.bashrc&&
-echo "alias begin='cd $HOME/cmdtypistversion1.0/'">> $HOME/.bashrc&&
-echo "alias cmdtypist='$HOME/cmdtypistversion1.0/cmdtypist'">> $HOME/.bashrc&
-cp -r ../cmdtypistversion1.0 $HOME/cmdtypistversion1.0/ 
+echo "Enter password to create a link for cmdtypist and access it from any directory" &&
+sudo gcc cmdtypist.c -o cmdtypist &&
+echo >> $HOME/.bashrc &&
+echo "#symbolic link for cmdtypist">> $HOME/.bashrc &&
+echo "alias begin='cd $HOME/cmdtypist/'">> $HOME/.bashrc &&
+echo "alias cmdtypist='$HOME/cmdtypist/cmdtypist'">> $HOME/.bashrc &&
+mkdir $HOME/cmtypist/
+cp -r . $HOME/cmtypist/
