98cbb06b37d4a988b60517340b47bd76964a1796
Calson Noah <chiacalson@gmail.com>
2017-05-31 09:08:28 +0100

    completed first test condition

--- cmdtypist.c
+++ cmdtypist.c
@@ -3,7 +3,17 @@
 void select_lesson(int argc_cmd)
 	{
 
-
-
+		if(argc_cmd>0&&argc_cmd<4)//checking on the command line argument.
+		{				
+			char ch;//
+			printf("%s", "Enter command >>");
+			while(scanf("%s",firstarg)!=1||(scanf("%d",&lesson_choice))!=1||lesson_choice<1||lesson_choice>15||strncmp(firstarg,"select",6)!=0)//Ensuring that "select" 
+			{       						//is entered accurately and the selected value is within the correct range.
+				                   
+			    if((strncmp(firstarg,"se",2)==0||strcmp(firstarg,"sel")==0||strcmp(firstarg,"sele")==0)
+			    											//Making suggestion to help user prevent errors.
+						fprintf(stderr, "\n%s\n", "Did you mean \"select usernumber\"");
+			}
+		}
 
 	}
