e44e2873b3dfa44a09796485ad7d8dde80c1e1af
Calson Noah <chiacalson@gmail.com>
2017-06-08 16:48:27 +0100

    comment edit

--- cmdtypist.c
+++ cmdtypist.c
@@ -18,7 +18,7 @@ void select_lesson(int argc_cmd)
 				else if((lesson_choice<1||lesson_choice>20)&&strcmp(firstarg,"select")==0)
 					fprintf(stderr, "%s %d\n", "No lesson entry for ",lesson_choice);
 				else printf("%s\n", "Command not found");
-				while(ch=getchar()!='\n');//flushing off wrong string
+				while(ch=getchar()!='\n');//disposing off wrong input string.
 			}
 		}
 		else
@@ -26,6 +26,34 @@ void select_lesson(int argc_cmd)
 			fprintf(stderr, "%s\n", "Invalid number of arguments, consult \"cmdtypist --help\" for more");
 			exit(EXIT_FAILURE);
 		}
-			
+			/*
+			if(argc_cmd==3)
+				if(strncmp(argv[2],"select",6)!=0||lesson_choice<1||lesson_choice>20)
+				{
+					fprintf(stderr, "%s\n", "Command not found\n");
+					if(strncmp(argv[2],"se",2))
+						fprintf(stderr, "%s\n", "Did you mean \"select\"");
+					else if(lesson_choice<1||lesson_choice>20)
+						fprintf(stderr, "%s %d\n", "No lesson entry for ",lesson_choice);
+					//else if(ch!=1)
+				        printf("%s", "Lesson number cannot contain symbols or alpha letters.\n");
+					exit(EXIT_FAILURE);
+				}*/
 		printf("\n");
 	}
+
+void urs_or_cchl(void)
+{
+	if(read_myown_config()==0)
+	{
+		strcpy(file_to_read,"my_own.txt");
+		mode=1;
+	}
+	else if(read_myown_config()==1)
+			strcpy(file_to_read,"noslaclessons.txt");
+	else
+	{
+		fprintf(stderr, "%s\n", "Fatal Error, lesson file corrupted or does not exist");
+		exit(EXIT_FAILURE);
+	}
+}
